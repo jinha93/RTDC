aa8cbcf8eb8d1f76eb76c302f1f2893675689d06
Filipe <jhhhhh93@naver.com>
2022-05-07 22:41:24 +0900

    '로고적용'

--- app/src/main/java/com/rtdc/controller/HomeController.java
+++ app/src/main/java/com/rtdc/controller/HomeController.java
@@ -3,6 +3,11 @@ package com.rtdc.controller;
 import java.io.IOException;
 import java.util.List;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,10 +25,36 @@ public class HomeController{
 	
 	@Autowired
 	private PostService postService;
+	
+	private final String mtdzUrl = "https://opensea.io/collection/mtdz-1";
+	private final String mtdzhref = "/collection/mtdz-1?search[sortAscending]=true&search[sortBy]=PRICE&search[toggles][0]=BUY_NOW";
+	private final String mtgUrl = "https://opensea.io/collection/meta-toy-gamers";
+	private final String mtghref = "/collection/meta-toy-gamers?search[sortAscending]=true&search[sortBy]=PRICE&search[toggles][0]=BUY_NOW";
+	private final String mtbUrl = "https://opensea.io/collection/meta-toy-bricks";
+	private final String mtbhref = "/collection/meta-toy-bricks?search[sortAscending]=true&search[sortBy]=PRICE&search[toggles][0]=BUY_NOW";
+	
+	
+	
+	private String getFp(String url, String href) throws IOException {
+		Connection connect = Jsoup.connect(url);
+		connect.header("User-Agent", "Chrome/101.0.4951.54");
+		
+		Document document = connect.get();
+		Elements elements = document.getElementsByAttributeValue("href", href);
+		String fp = elements.text().toString();
+		fp = fp.replaceAll("[^0-9]", "");
+		return fp;
+	}
+	
 
 	
     @GetMapping("/")
     public String index(Model model) throws IOException{
+    	
+    	model.addAttribute("mtdzFp",getFp(mtdzUrl, mtdzhref));
+    	model.addAttribute("mtgFp",getFp(mtgUrl, mtghref));
+    	model.addAttribute("mtbFp",getFp(mtbUrl, mtbhref));
+    	
     	List<Post> popularPostList = postService.getPopularPostList();
     	List<Post> newPostList = postService.getNewPostList();
 		
